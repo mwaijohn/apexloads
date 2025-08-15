db0718d7465898657c0c95ef0a4c570d6a194914
john john <john@mail.com>
2025-08-15 12:38:39 +0300

    redesigned

--- src/app/components/postdetails/PostingDetailsCard.tsx
+++ src/app/components/postdetails/PostingDetailsCard.tsx
@@ -5,20 +5,20 @@ const PostingDetailsCard = () => {
         <div className="border rounded shadow bg-[#272627] border-[#3a3a3a] mt-6">
             <h4 className="text-sm font-medium mb-3 bg-[#3a3a3b] px-3 py-2 rounded">POSTING DETAILS</h4>
             <div className="space-y-2 text-sm px-3">
-                <div className="flex justify-between">
-                    <span className="text-gray-400">Post to</span>
+                <div className="flex justify-start gap-4">
+                    <span className="text-gray-400 w-20">Post to</span>
                     <span>Private Network</span>
                 </div>
             </div>
             <div className="mt-6 px-3 pb-3">
                 <div className="space-y-2 text-sm">
                     <h4 className="text-sm font-medium mb-3">BOOKING</h4>
-                    <div className="flex justify-between">
-                        <span className="text-gray-400">Booking</span>
+                    <div className="flex justify-start gap-4">
+                        <span className="text-gray-400 w-20">Booking</span>
                         <span>Book Instantly</span>
                     </div>
-                    <div className="flex justify-between">
-                        <span className="text-gray-400">Bidding</span>
+                    <div className="flex justify-start gap-4">
+                        <span className="text-gray-400 w-20">Bidding</span>
                         <span>Allow for Private Network</span>
                     </div>
                 </div>
--- src/app/components/postdetails/RateCard.tsx
+++ src/app/components/postdetails/RateCard.tsx
@@ -5,20 +5,19 @@ const RateCard = () => {
         <div className="border rounded shadow bg-[#272627] border-[#3a3a3a]">
             <h3 className="text-sm font-medium mb-4 bg-[#3a3a3b] px-3 py-2">RATE</h3>
             <div className="space-y-2 text-sm px-3 pb-3">
-                <div className="flex justify-between">
-                    <span className="text-gray-400">Total</span>
+                <div className="flex justify-start gap-4">
+                    <span className="text-gray-400 w-20">Total</span>
                     <span>$ 3,500</span>
                 </div>
-                <div className="flex justify-between">
-                    <span className="text-gray-400">Trip</span>
+                <div className="flex justify-start gap-4">
+                    <span className="text-gray-400 w-20">Trip</span>
                     <span>1098 mi</span>
                 </div>
-                <div className="flex justify-between">
-                    <span className="text-gray-400">Rate / mile</span>
+                <div className="flex justify-start gap-4">
+                    <span className="text-gray-400 w-20">Rate / mile</span>
                     <span>$ 2.000</span>
                 </div>
             </div>
-
         </div>
     )
 }
